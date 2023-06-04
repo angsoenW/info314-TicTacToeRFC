cfb8ceeec008a8daf31685fa0bea2a128912faca
angsoenW <91903037+angsoenW@users.noreply.github.com>
2023-06-04 04:19:40 -0700

    implemented methods in server

--- Client/Player.java
+++ Client/Player.java
@@ -4,28 +4,37 @@ import java.util.*;
 
 public class Player {
     public static void main(String[] args) throws IOException {
+        
         Socket socket = new Socket("localhost", 3116);
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter your User Name: ");
+
         String client = scanner.next();
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
         String message = "HELO " + 1 + " " + client;
         System.out.println("Sending the Greeting messages to the server: " + message);
+
         out.println(message);
+
         String serverResponse = in.readLine();
         String[] numbers = serverResponse.split(" ");
         String client_id = numbers[numbers.length - 1];
+
         System.out.println("This is your client ID: " + client_id);
         System.out.print("You want to join a game or create a new game: ");
+
         String action = scanner.next();
+
         if (action.equals("create")) {
             System.out.println("Starting to create a new game: ");
             out.println("CREA " + client_id);
             System.out.print("Wait the other user...");
             String current = in.readLine();
             System.out.println("Status: " + current);
-        } else {
+        } else if (action.equals("join")) {
             System.out.print("This is all games that are available: ");
             out.println("LIST");
             String games = in.readLine();
@@ -37,6 +46,7 @@ public class Player {
             String current = in.readLine();
             System.out.println(current);
         }
+        
         in.close();
         out.close();
         socket.close();
